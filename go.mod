bd2a58fa7987bc3e476c210e67d6518899c15e25
zvdy <cristian.030902@gmail.com>
2023-09-12 09:20:18 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/zvdy/go-telegram-bot
+
+go 1.22
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-telegram/bot v0.7.15 // indirect
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	botToken := os.Getenv("BOT_TOKEN")
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	b, err := bot.New(botToken, opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
+}
